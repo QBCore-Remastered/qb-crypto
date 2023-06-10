0d6c68d5ad4bfa4e8ed1d3df207598901030cb10
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-06-10 15:43:47 +0200

    cleanup(config): general stuff

--- config.lua
+++ config.lua
@@ -1,20 +1,20 @@
 Crypto = {
-        Lower = 500,
-        Upper = 5000,
+    Lower = 500,
+    Upper = 5000,
     History = {
-        ["qbit"] = {}
+        qbit = {}
     },
 
     Worth = {
-        ["qbit"] = 1000
+        qbit = 1000
     },
 
     Labels = {
-        ["qbit"] = "Qbit"
+        qbit = "Qbit"
     },
 
     Exchange = {
-        coords = vector3(1276.21, -1709.88, 54.57),
+        coords = vec3(1276.21, -1709.88, 54.57),
         RebootInfo = {
             state = false,
             percentage = 0
@@ -27,21 +27,18 @@ Crypto = {
 
     -- Crashes or luck
     ChanceOfCrashOrLuck = 2, -- This is in % (1-100)
-    Crash = {20,80}, -- Min / Max
-    Luck = {20,45}, -- Min / Max
+    Crash = {20, 80}, -- Min / Max
+    Luck = {20, 45}, -- Min / Max
 
     -- If not not Chance of crash or luck, then this shit
     ChanceOfDown = 30, -- If out of 100 hits less or equal to
     ChanceOfUp = 60, -- If out of 100 is greater or equal to
-    CasualDown = {1,10}, -- Min / Max (If it goes down)
-    CasualUp = {1,10}, -- Min / Max (If it goes up)
+    CasualDown = {1, 10}, -- Min / Max (If it goes down)
+    CasualUp = {1, 10}, -- Min / Max (If it goes up)
 }
 
-
-
-
 Ticker = {
-    Enabled = false, -- Decide whether the real life price ticker should be enabled or not :)  
+    Enabled = false, -- Decide whether the real life price ticker should be enabled or not :)
     coin = 'BTC', --- The coin, please make sure you find the actual name, for example: Bitcoin vs BTC, BTC would be correct
     currency = 'USD', -- For example USD, NOK, SEK, EUR, CAD and more here https://www.countries-ofthe-world.com/world-currencies.html
     tick_time = 2, --- Minutes (Minimum is 2 minutes) 20,160 Requests a month, Its recommended to get the free API key so the crypto script doesnt switch on and off if ratelimit is encountered
