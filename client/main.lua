78b3ae864f4d635f204b7fb8a7e33a89eea07143
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-10-10 18:07:37 -0400

    chore: accommodate new core (#3)

--- client/main.lua
+++ client/main.lua
@@ -1,23 +1,4 @@
--- Variables
-local QBCore = exports['qbx-core']:GetCoreObject()
-
 -- Functions
-
-local function DrawText3Ds(coords, text)
-	SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(true)
-    SetTextColour(255, 255, 255, 215)
-    BeginTextCommandDisplayText("STRING")
-    SetTextCentre(true)
-    AddTextComponentSubstringPlayerName(text)
-    SetDrawOrigin(coords.x, coords.y, coords.z, 0)
-    EndTextCommandDisplayText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
-
 local function ExchangeSuccess()
 	TriggerServerEvent('qb-crypto:server:ExchangeSuccess', math.random(1, 10))
 end
@@ -65,7 +46,7 @@ local point = lib.points.new({
 
 function point:nearby()
 	if not Crypto.Exchange.RebootInfo.state then
-		DrawText3Ds(Crypto.Exchange.coords, Lang:t('text.enter_usb'))
+		DrawText3D(Lang:t('text.enter_usb'), Crypto.Exchange.coords)
 
 		if IsControlJustPressed(0, 38) then
 			local HasItem = exports.ox_inventory:Search('count', 'cryptostick') >= 1
@@ -73,11 +54,11 @@ function point:nearby()
 				TriggerEvent("mhacking:show")
 				TriggerEvent("mhacking:start", math.random(4, 6), 45, HackingSuccess)
 			else
-				QBCore.Functions.Notify(Lang:t('error.you_dont_have_a_cryptostick'), 'error')
+				exports.qbx_core:Notify(Lang:t('error.you_dont_have_a_cryptostick'), 'error')
 			end
 		end
 	else
-		DrawText3Ds(Crypto.Exchange.coords, Lang:t('text.system_is_rebooting', {rebootInfoPercentage = Crypto.Exchange.RebootInfo.percentage}) )
+		DrawText3D(Lang:t('text.system_is_rebooting', {rebootInfoPercentage = Crypto.Exchange.RebootInfo.percentage}), Crypto.Exchange.coords)
 	end
 end
 
